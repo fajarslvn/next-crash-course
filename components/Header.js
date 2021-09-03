bf4021cacc16b79156b82387d0be6753dfd03f9e
fajarslvn <jarphish@gmail.com>
2021-09-03 08:24:15 +0700

    init

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import headerStyles from '../styles/Header.module.css'
+
+const Header = () => {
+  return (
+    <Header className={headerStyles.title}>
+      
+    </Header>
+  )
+}
+
+export default Header
